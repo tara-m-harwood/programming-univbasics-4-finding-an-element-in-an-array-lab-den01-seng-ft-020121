f78cadcf0e657e5715e149e50e2e304d0185b3d7
Tara Harwood <tara.m.harwood@gmail.com>
2021-01-09 13:05:20 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -3,7 +3,7 @@ def find_element_index(array, value_to_find)
   found_value_index = nil
   while count < array.length {
     if array[count] == value_to_find
-      count += 1
+      found_value_index = count
   }
   array
 end
